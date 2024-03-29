2217872595a6a0d64fa725c727e1c207f05835ce
aliasGitee <sizukululu@163.com>
2024-03-29 18:44:54 +0800

    fix

--- basicsr/archs/safmn_arch.py
+++ basicsr/archs/safmn_arch.py
@@ -8,8 +8,8 @@ from basicsr.utils.registry import ARCH_REGISTRY
 
 @ARCH_REGISTRY.register()
 class SAFMN(nn.Module):
-    def __init__(self):
+    def __init__(self,dim=36, n_blocks=8, ffn_scale=2.0, upscaling_factor=2):
         super(SAFMN, self).__init__()
-        self.model = model(dim=36, n_blocks=8, ffn_scale=2.0, upscaling_factor=2)
+        self.model = model(dim, n_blocks, ffn_scale, upscaling_factor)
     def forward(self, x):
         return self.model(x)
