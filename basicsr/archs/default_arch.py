840fa1dbaccd824582e5fcc1f920023b2eb60c48
aliasGitee <sizukululu@163.com>
2024-03-30 20:35:00 +0800

    fix

--- basicsr/archs/default_arch.py
+++ basicsr/archs/default_arch.py
@@ -1,7 +1,10 @@
 from basicsr.archs.myarchs import myfix2
 import torch
 import torch.nn as nn
+from basicsr.utils.registry import ARCH_REGISTRY
 
+
+@ARCH_REGISTRY.register()
 class DefaultArch(nn.Module):
     def __init__(self):
         super().__init__()
